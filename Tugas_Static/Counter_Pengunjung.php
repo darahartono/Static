3b0d38b81d916dfe2e28297b7e20c94e3ac45293
darahartono <darahartono63@gmail.com>
2026-04-22 17:00:48 +0700

    before

--- Tugas_Static/Counter_Pengunjung.php
+++ Tugas_Static/Counter_Pengunjung.php
@@ -14,7 +14,7 @@ $p1 = new Pengunjung();
 $p2 = new Pengunjung();
 $p3 = new Pengunjung();
 $p4 = new Pengunjung();
-$p5 = new Pengunjung();
+$p5 = new Pengunjung(); 
 
 echo "jumlah Pengunjung: " . Pengunjung::$jumlah
 
